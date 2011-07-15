8dccae9d460aca15aedba1478524a058df7807b9
Wobba_cp <SND\Wobba_cp>
2011-07-15 08:22:18 +0000

    Initial check-in

---
+++ Development/KqlParser/Token.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KQLParser
+{
+    [Flags]
+    public enum TokenType
+    {
+        Operator = 1, Phrase = 2, Group = 4, Word = 8, Property = 16
+    }
+
+    class Token
+    {
+        public string Text { get; set; }
+        public TokenType Type { get; set; }
+        public string ParentOperator { get; set; }
+
+        public string GetFql(Synonym synonym)
+        {
+            if (Type == TokenType.Phrase || Type == TokenType.Word || Type == TokenType.Property)
+            {
+                return FqlHelper.GetFqlQueryForTerm(this);
+            }
+            else if (Type == TokenType.Group)
+            {
+                Text = Text.Trim(new char[] { '(', ')', ' ' });
+                TokenType allowed;
+                if (ParentOperator == "ANY" || ParentOperator == "ALL" && ParentOperator == "NONE" && ParentOperator == "NOT")
+                    allowed = TokenType.Phrase | TokenType.Property | TokenType.Word;
+                else
+                    allowed = TokenType.Phrase | TokenType.Property | TokenType.Word | TokenType.Operator;
+                TokenBuilder builder = new TokenBuilder(Text, allowed);
+                builder.Build();
+
+                if (ParentOperator == "ANY")
+                {
+                    builder.OrExpr.AddRange(builder.AndExpr);
+                    builder.AndExpr.Clear();
+                }
+
+                List<string> includes = new List<string>();
+                List<string> excludes = new List<string>();
+                FqlHelper helper = new FqlHelper();
+                helper.CreateTokenFql(builder, includes, excludes, synonym);
+                return helper.Build(includes, excludes);
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ Development/KqlParserTest/Kql2FqlTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using KQLParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KQLParserTest
+{
+    [TestClass]
+    public class Kql2FqlTest
+    {
+        FqlHelper _helper = new FqlHelper();
+
+        [TestMethod]
+        public void PropertyWord()
+        {
+            string query = @"author:mikael";
+            string result = _helper.GetFqlFromKql(query);
+        }
+
+        [TestMethod]
+        public void PropertyPhrase()
+        {
+            string query = @"author:""mikael svenson""";
+            string result = _helper.GetFqlFromKql(query);
+        }
+
+        [TestMethod]
+        public void MultiplePropertyPhraseSame1()
+        {
+            string query = @"author:""mikael svenson"" author:""svenson mikael""";
+            string result = _helper.GetFqlFromKql(query);
+        }
+
+        [TestMethod]
+        public void MultiplePropertyPhraseSame2()
+        {
+            string query = @"author:""mikael svenson"" author:""svenson mikael"" ANY(c d)";
+            string result = _helper.GetFqlFromKql(query);
+        }
+
+        [TestMethod]
+        public void MultiplePropertyPhraseSame3()
+        {
+            string query = @"ANY(author:""mikael svenson"" author:""svenson mikael"") c OR d";
+            string result = _helper.GetFqlFromKql(query);
+        }
+
+        [TestMethod]
+        public void MultiplePropertyPhraseSame4()
+        {
+            string query = @"author:a author:b author:c c OR d";
+            string result = _helper.GetFqlFromKql(query);
+        }
+
+        [TestMethod]
+        public void Synonym1()
+        {
+            string query = @"-pepsi solo";
+            string result = _helper.GetFqlFromKql(query, Synonym.Include, 100);
+        }
+
+        [TestMethod]
+        public void Synonym2()
+        {
+            string query = @"test NONE(pepsi)";
+            string result = _helper.GetFqlFromKql(query, Synonym.Include, 100);
+        }
+        
+        [TestMethod]
+        public void MultiplePropertyPhraseDifferent()
+        {
+            string query = @"author:""mikael svenson"" name:""svenson mikael""";
+            string result = _helper.GetFqlFromKql(query);
+        }
+
+        [TestMethod]
+        public void MultiTest1()
+        {
+            string query = "(\"SharePoint Search\" OR \"Live Search\") AND title:\"Keyword Syntax\" NOT (sug OR svelg)";
+            string result = _helper.GetFqlFromKql(query);
+        }
+        //("SharePoint Search" OR "Live Search") AND title:"Keyword Syntax" NOT (sug OR svelg)
+        //string query = @"(""SharePoint Search"" OR ""Live Search"") AND title:""title title"" NOT (sug OR svelg) ANY(test test2) ""mikael svenson"" ";
+        //string query = @"(""SharePoint Search"" OR ""Live Search"") title:""title title"" NOT (sug OR svelg) ANY(test test2) ""mikael svenson"" ";
+        //string query = @"ANY(test test2) ""mikael svenson"" ";
+        //string query = @"NOT(a) ANY(test test2)";
+
+    }
+}
