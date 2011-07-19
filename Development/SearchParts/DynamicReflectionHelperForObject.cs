14796d8af5315ce8e54b752b2dc3ced18ad20107
Mikael Svenson <51104+wobba@users.noreply.github.com>
2011-07-19 20:16:56 +0000

    Changed wsp name.

--- Development/SearchParts/DynamicReflectionHelperForObject.cs
+++ Development/SearchParts/DynamicReflectionHelperForObject.cs
@@ -17,11 +17,12 @@ namespace mAdcOW.SharePoint.Search
     public class DynamicReflectionHelperforObject<TV>
     {
         public delegate T GetPropertyFieldDelegate<T>(TV obj);
+        public delegate void SetPropertyFieldDelegate(object obj, object value);
 
         public static GetPropertyFieldDelegate<TC> GetProperty<TC>(string memberName)
         {
             Type v = typeof (TV);
-            PropertyInfo pi = v.GetProperty(memberName, BindingFlags.NonPublic | BindingFlags.Instance);
+            PropertyInfo pi = v.GetProperty(memberName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
             if (pi == null)
                 throw new NullReferenceException("No Property or Field");
 
@@ -32,6 +33,40 @@ namespace mAdcOW.SharePoint.Search
             il.EmitCall(OpCodes.Call, pi.GetGetMethod(true), null);
             il.Emit(OpCodes.Ret);
             return (GetPropertyFieldDelegate<TC>) dm.CreateDelegate(typeof (GetPropertyFieldDelegate<TC>));
+        }
+
+        public delegate void SetPropertyFieldDelegate<T>(TV obj, T m_Value);
+
+        public static SetPropertyFieldDelegate<C> SetP<C>(string memberName, C mValue)
+        {
+            try
+            {
+                Type v = typeof(TV);
+                PropertyInfo pi = v.GetProperty(memberName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                FieldInfo fi = v.GetField(memberName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                if (pi != null || fi != null)
+                {
+                    DynamicMethod dm = new DynamicMethod("SetPropertyorField_" + memberName, typeof(void),
+                        new Type[] { v, typeof(C) }, v.Module);
+                    ILGenerator il = dm.GetILGenerator();
+                    il.Emit(OpCodes.Ldarg_0);
+                    il.Emit(OpCodes.Ldarg_1);
+                    if (pi != null)
+                        il.EmitCall(OpCodes.Callvirt, pi.GetSetMethod(true), new Type[] { typeof(C) });
+                    else if (fi != null)
+                        il.Emit(OpCodes.Stfld, fi);
+                    il.Emit(OpCodes.Ret);
+
+                    return (SetPropertyFieldDelegate<C>)dm.CreateDelegate(typeof(SetPropertyFieldDelegate<C>));
+                }
+                else
+                    throw new NullReferenceException("No Property or Field");
+
+            }
+            catch (Exception ex)
+            {                
+                throw ex;
+            }
         }
     }
 }
