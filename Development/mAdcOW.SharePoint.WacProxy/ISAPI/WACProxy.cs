bf60b5b6303c2ac13c796b55af3fe2a0af0d3e20
Wobba_cp <SND\Wobba_cp>
2013-01-09 08:27:59 +0000

    Added WACProxy running elevated

---
+++ Development/mAdcOW.SharePoint.WacProxy/ISAPI/WACProxy.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+
+using Microsoft.SharePoint;
+
+namespace mAdcOW.SharePoint
+{
+    public class WACProxy : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            SPSecurity.RunWithElevatedPrivileges(delegate()
+            {
+                var proxy = new Microsoft.Office.Server.Search.Extended.Query.Internal.UI.WACProxy();
+                proxy.ProcessRequest(context);
+            });
+
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+    }
+}
