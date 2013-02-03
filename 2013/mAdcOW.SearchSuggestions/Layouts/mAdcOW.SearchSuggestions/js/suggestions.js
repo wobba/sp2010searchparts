347fef2ff8900c4ddacb6c830209207fa2cde8c0
Wobba_cp <SND\Wobba_cp>
2013-02-03 20:57:31 +0000

    Added feature for master page. Added java scripts

---
+++ 2013/mAdcOW.SearchSuggestions/Layouts/mAdcOW.SearchSuggestions/js/suggestions.js
@@ -0,0 +1,74 @@
+﻿ExecuteOrDelayUntilScriptLoaded(Override, 'ajaxtoolkit.js');
+function Override() {
+    AjaxControlToolkit.AutoCompleteBehavior.prototype._getSuggestion = function() {
+        var cctx = Srch.ScriptApplicationManager.get_clientRuntimeContext();
+        try {
+            var text = this._currentPrefix;
+            var lang = Srch.AU.get_querySuggestionLanguage();
+            var sendInfo = {
+                url: cctx.get_url(),
+                sourceId: this._sourceId,
+                query: this._currentPrefix,
+                language: lang,
+                numberOfQuerySuggestions: this._queryCount,
+                numberOfResultSuggestions: this._personalResultCount,
+                preQuerySuggestions: true,
+                hitHighlighting: true,
+                showPeopleNameSuggestions: this._showPeopleNameSuggestions,
+                capitalizeFirstLetters: this._showPeopleNameSuggestions,
+                prefixMatchAllTerms: false
+            };
+
+            try {
+                // this is where I want to update
+
+                var queryArr = [];
+                var personalArr = [];
+                var peopleArr;
+
+                jQuery.ajax({
+                    url: '_vti_bin/SearchSuggestions.ashx',
+                    data: sendInfo,
+                    async: false,
+                    dataType: "json",
+                    success: function(data) {
+                        $.each(data.Queries, function() {
+                            var suggestion = new Microsoft.SharePoint.Client.Search.Query.QuerySuggestionQuery();
+                            suggestion.set_query(this.Query);
+                            suggestion.set_isPersonal(this.IsPersonal);
+                            queryArr.push(suggestion);
+                        });
+                        var suggestion = new Microsoft.SharePoint.Client.Search.Query.PersonalResultSuggestion();
+                        $.each(data.PersonalResults, function() {
+                            var personal = new Microsoft.SharePoint.Client.Search.Query.PersonalResultSuggestion();
+                            personal.set_highlightedTitle(this.HighlightedTitle);
+                            personal.set_isBestBet(this.IsBestBet);
+                            personal.set_title(this.Title);
+                            personal.set_url(this.Url);
+                            personalArr.push(personal);
+                        });
+
+                        peopleArr = data.PeopleNames;
+                    },
+                    error: function() {
+                        //response( [] );
+                    }
+                });
+
+                var newResult = new Microsoft.SharePoint.Client.Search.Query.QuerySuggestionResults();
+                newResult.set_queries(queryArr);
+                newResult.set_peopleNames(peopleArr);
+                newResult.set_personalResults(personalArr)
+
+                this._update(text, newResult, true);
+            } catch(ex) {
+                Srch.U.trace(null, "AutoCompleteBehavior._update", ex.toString());
+            }
+            //            }
+        } catch(e) {
+            Srch.U.trace(null, "AutoCompleteBehavior._getSuggestion", e.toString());
+        }
+
+        $common.updateFormToRefreshATDeviceBuffer();
+    };
+}
